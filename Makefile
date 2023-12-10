3b60eb93e6a17cb19e21e4e4ebae6fcd0433dfc8
biatl0n <biatl0n@rockyDesk.local>
2023-12-11 02:16:12 +0500

    Подключен Bootstrap, Twig, создан шаблон главной страницы

--- Makefile
+++ Makefile
@@ -7,7 +7,13 @@ validate:
 	composer validate
 
 lint:
-	composer exec --verbose phpcs -- --standard=PSR12 public
+	composer exec --verbose phpcs -- --standard=PSR12 --ignore='/public/styles/*, /public/scripts/*' public 
 
 start:
 	PHP_CLI_SERVER_WORKERS=5 php -S 0.0.0.0:$(PORT) -t public
+
+build_docker_image:
+	docker build --tag 'page_analizer' .
+
+start_in_docker:
+	docker run -it -p 127.0.0.1:8000:8000 'page_analizer'
--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
 <?php
 
-print_r("Test");
+require __DIR__ . '/../vendor/autoload.php';
+
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set('view', function () {
+    return Twig::create(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::create();
+$app->add(TwigMiddleware::createFromContainer($app));
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$header = __DIR__ . "/../templates/header.phtml";
+
+$app->get('/', function ($request, $response) {
+    return $this->get('view')->render($response, 'index.phtml');
+})->setName('index');
+
+$app->run();
